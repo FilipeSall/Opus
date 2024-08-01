098e8c1b39e10124728b481b9476923d0ea85e62
Filipe Salles <Filipesallesdev@gmail.com>
2024-08-01 20:55:09 -0300

    Colocando meta tags melhores e alterando o footer para ficar mais atraente

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,22 +3,22 @@ import ContactIcons from './ContactIcons';
 import './footer.css';
 
 function Footer() {
-  return (
-   <footer className='footer__container'>
-        <div className='footer__container-infos'>
-            <div className='container-info'>
-                <h3>Telefone</h3>
-                <p>(61) 9.99827-2992</p>
+    return (
+        <footer className='footer__container'>
+            <div className='footer__container-infos'>
+                <div className='container-info'>
+                    <h3>Telefone</h3>
+                    <p>(61) 9.99827-2992</p>
+                </div>
+                <div className='container-info'>
+                    <h3>Email</h3>
+                    <p>Luizgraciliano@hotmail.com</p>
+                    <p>Luizgraciliano52@gmail.com</p>
+                </div>
             </div>
-            <div className='container-info'>
-                <h3>Email</h3>
-                <p>Luizgraciliano@hotmail.com</p>
-                <p>Luizgraciliano52@gmail.com</p>
-            </div>
-        </div>
-        <ContactIcons />
-   </footer>
-  )
+            <ContactIcons />
+        </footer>
+    )
 }
 
 export default Footer;
